eeff79288617ba2c7b2fd6a1e836687f9238aaa4
Martônio Júnior <martoniojunior@hotmail.com>
2022-11-07 19:09:51 -0300

    change(CameraManager): Updated to use GyroCamera

--- Assets/_Project/Player/CameraManager.cs
+++ Assets/_Project/Player/CameraManager.cs
@@ -1,6 +1,7 @@
 using ENA.Input;
 using ENA.Persistency;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 namespace ENA.Player
 {
@@ -8,34 +9,21 @@ namespace ENA.Player
     {
         #region Variables
         [SerializeField] PlayerController player;
+        [SerializeField] GyroCamera gyroscope;
         [Header("Camera Handlers")]
-        [SerializeField] ControleGiroscopio defaultCameraGyro;
-        [SerializeField] ControleGiroscopio vrCameraGyro;
+        [SerializeField] GameObject defaultCamera;
+        [SerializeField] GameObject vrCamera;
         #endregion
         #region Methods
         public void SetCameraGyro(bool value)
         {
-            vrCameraGyro.enabled = value;
-            defaultCameraGyro.enabled = value;
-
-            if (value) {
-                Transform.localEulerAngles = new Vector3(90, 0, 0);
-                Transform.SetParent(player.Transform.parent);
-            } else {
-                Transform.localEulerAngles = new Vector3(0, 0, 0);
-            }
+            gyroscope.enabled = value;
         }
 
         public void SetVR(bool value)
         {
-            if (value) {
-                player.SetCameraTransform(vrCameraGyro.transform);
-            } else {
-                player.SetCameraTransform(defaultCameraGyro.transform);
-            }
-
-            defaultCameraGyro.gameObject.SetActive(!value);
-            vrCameraGyro.gameObject.SetActive(value);
+            defaultCamera?.SetActive(!value);
+            vrCamera?.SetActive(value);
         }
         #endregion
     }
