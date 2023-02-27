11b039a9f1769966218a790593524b39cf4aabaa
Martônio Júnior <martoniojunior@hotmail.com>
2023-02-27 19:05:46 -0300

    chore(GyroCamera): Conforming to variable conventions

--- Assets/_Project/Input/GyroCamera.cs
+++ Assets/_Project/Input/GyroCamera.cs
@@ -6,14 +6,14 @@ namespace ENA.Input
     public class GyroCamera: ExtendedMonoBehaviour
     {
         #region Variables
-        private float _initialYAngle = 0f;
-        private float _appliedGyroYAngle = 0f;
-        private float _calibrationYAngle = 0f;
-        private Transform _rawGyroRotation;
-        private float _tempSmoothing;
+        private float initialYAngle = 0f;
+        private float appliedGyroYAngle = 0f;
+        private float calibrationYAngle = 0f;
+        private Transform rawGyroRotation;
+        private float tempSmoothing;
         private Gyroscope gyro;
         [Header("Settings")]
-        [SerializeField] private float _smoothing = 0.1f;
+        [SerializeField] private float smoothing = 0.1f;
         #endregion
         #region MonoBehaviour Lifecycle
         private IEnumerator Start()
@@ -23,10 +23,10 @@ namespace ENA.Input
 				gyro.enabled = true;
 
                 Application.targetFrameRate = 60;
-                _initialYAngle = Transform.eulerAngles.y;
+                initialYAngle = Transform.eulerAngles.y;
 
-                _rawGyroRotation = new GameObject("GyroRaw").transform;
-                _rawGyroRotation.SetPositionAndRotation(Transform.position, Transform.rotation);
+                rawGyroRotation = new GameObject("GyroRaw").transform;
+                rawGyroRotation.SetPositionAndRotation(Transform.position, Transform.rotation);
 
                 // Wait until gyro is active, then calibrate to reset starting rotation.
                 yield return new WaitForSeconds(1);
@@ -44,21 +44,21 @@ namespace ENA.Input
             ApplyGyroRotation();
             ApplyCalibration();
 
-            Transform.rotation = Quaternion.Slerp(Transform.rotation, _rawGyroRotation.rotation, _smoothing);
+            Transform.rotation = Quaternion.Slerp(Transform.rotation, rawGyroRotation.rotation, smoothing);
         }
         #endregion
         #region Methods
         private void ApplyGyroRotation()
         {
-            _rawGyroRotation.rotation = UnityEngine.Input.gyro.attitude;
-            _rawGyroRotation.Rotate(0f, 0f, 180f, Space.Self); // Swap "handedness" of quaternion from gyro.
-            _rawGyroRotation.Rotate(90f, 180f, 0f, Space.World); // Rotate to make sense as a camera pointing out the back of your device.
-            _appliedGyroYAngle = _rawGyroRotation.eulerAngles.y; // Save the angle around y axis for use in calibration.
+            rawGyroRotation.rotation = UnityEngine.Input.gyro.attitude;
+            rawGyroRotation.Rotate(0f, 0f, 180f, Space.Self); // Swap "handedness" of quaternion from gyro.
+            rawGyroRotation.Rotate(90f, 180f, 0f, Space.World); // Rotate to make sense as a camera pointing out the back of your device.
+            appliedGyroYAngle = rawGyroRotation.eulerAngles.y; // Save the angle around y axis for use in calibration.
         }
 
         private void ApplyCalibration()
         {
-            _rawGyroRotation.Rotate(0f, -_calibrationYAngle, 0f, Space.World); // Rotates y angle back however much it deviated when calibrationYAngle was saved.
+            rawGyroRotation.Rotate(0f, -calibrationYAngle, 0f, Space.World); // Rotates y angle back however much it deviated when calibrationYAngle was saved.
         }
 
         public void SetEnabled(bool value)
@@ -70,11 +70,11 @@ namespace ENA.Input
         #region Coroutines
         private IEnumerator CalibrateYAngle()
         {
-            _tempSmoothing = _smoothing;
-            _smoothing = 1;
-            _calibrationYAngle = _appliedGyroYAngle - _initialYAngle; // Offsets the y angle in case it wasn't 0 at edit time.
+            tempSmoothing = smoothing;
+            smoothing = 1;
+            calibrationYAngle = appliedGyroYAngle - initialYAngle; // Offsets the y angle in case it wasn't 0 at edit time.
             yield return null;
-            _smoothing = _tempSmoothing;
+            smoothing = tempSmoothing;
         }
         #endregion
     }
