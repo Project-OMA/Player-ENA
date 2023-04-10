4b2a35adbe103b6252a70dbedc3d60f2668b2209
Martônio Júnior <martoniojunior@hotmail.com>
2023-04-10 14:54:59 -0300

    change(CameraManager): Removed dependency to PlayerController

--- Assets/_Project/Player/CameraManager.cs
+++ Assets/_Project/Player/CameraManager.cs
@@ -8,13 +8,29 @@ namespace ENA.Player
     public class CameraManager: ExtendedMonoBehaviour
     {
         #region Variables
-        [SerializeField] PlayerController player;
         [SerializeField] GyroCamera gyroscope;
+        [SerializeField] SettingsProfile profile;
         [Header("Camera Handlers")]
         [SerializeField] GameObject defaultCamera;
         [SerializeField] GameObject vrCamera;
         #endregion
+        #region MonoBehaviour Lifecycle
+        /// <summary>
+        /// Start is called on the frame when a script is enabled just before
+        /// any of the Update methods is called the first time.
+        /// </summary>
+        void Start()
+        {
+            Configure(profile);
+        }
+        #endregion
         #region Methods
+        public void Configure(SettingsProfile profile)
+        {
+            SetVR(profile.VREnabled);
+            SetCameraGyro(profile.GyroEnabled);
+        }
+
         public void SetCameraGyro(bool value)
         {
             gyroscope.enabled = value;
